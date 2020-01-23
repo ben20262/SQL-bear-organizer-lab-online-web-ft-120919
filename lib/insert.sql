9fa41fa367cc4a8cb3f0815fba62e599bb0823c1
Benjamin Small <ben20262@aim.com>
2020-01-23 03:14:30 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "male", "brown", "chaotic evil", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 13, "male", "black", "chaotic neutral", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 14, "female", "brown", "chaotic good", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 15, "male", "brown", "lawful evil", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 16, "female", "black", "goofy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 17, "male", "albino", "true neutral", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 18, "female", "spotted", "kinda ok", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 19, "male", "brown", "chaotic evil", 0);
